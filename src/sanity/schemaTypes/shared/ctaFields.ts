8a4af7375a689f5590a63ac499febc17ad461d69
lawal-olaotan <olaotan104@gmail.com>
2026-03-14 12:42:23 +0000

    Feat: Enhance post and step types with introduction CTA and step CTA fields; add shared ctaFields schema

---
+++ src/sanity/schemaTypes/shared/ctaFields.ts
@@ -0,0 +1,14 @@
+import { defineField } from 'sanity'
+
+export const ctaFields = [
+  defineField({
+    name: 'ctaText',
+    title: 'CTA Text',
+    type: 'string',
+  }),
+  defineField({
+    name: 'ctaUrl',
+    title: 'CTA URL',
+    type: 'url',
+  }),
+]
